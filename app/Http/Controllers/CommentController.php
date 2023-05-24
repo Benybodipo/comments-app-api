34e22f6c56bd8cbb08a412092f73fab0a2dae5d9
benybodipo <benybodipo@gmail.com>
2023-05-24 16:19:15 +0200

    First commit

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try {
+            $comments = Comment::get();
+    
+            return response()->json( $comments);
+        } catch (\Throwable $th) {
+            return response()->json($e->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $validation = Validator::make($request->all(), [
+                'username' => 'required|min:2',
+                'body' => 'required|min:50'
+            ]);
+
+            if ($validation->fails()) {
+                return response()->json([
+                    'errors' => $validation->errors()
+                ], 400);
+            }
+    
+            $comment = Comment::create($request->all());
+    
+            return response()->json($comment);
+        } catch (\Throwable $e) {
+            return response()->json($e->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $comment = Comment::whereId($id)->first();
+        $code = (!empty($comment)) ? 200 : 404;
+
+        return response()->json($comment, $code);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Comment $comment)
+    {
+        //
+    }
+}
